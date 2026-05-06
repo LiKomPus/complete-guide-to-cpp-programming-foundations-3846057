662073aacdb2bf4ea386995ab049fc0047739eda
LiKomPus <56884876+LiKomPus@users.noreply.github.com>
2026-05-06 10:56:14 +0000

    Added solution

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -2,12 +2,23 @@
 // Exercise 01_02 with modifications
 // Hello World, by Eduardo Corpeño 
 
-#include <iostream>
+#include <iostream> // allows to write stuff to terminal easily.
+#include <cstdio>
 
-int main() {
+/*int main() {
     // cout, send text to terminal
     // endl, end line
     std::cout << "Hello world!" << std::endl;
-    std::cout << std::endl;
+    std::cout << std::endl << std::endl;
     return 0;
-}
+}*/
+
+/*int main() {
+    printf("Hello world, again!\n\n\n");
+    return 0;
+}*/
+
+int main(int argc, char* argv[]){
+    std::cout << "Hello world!" <<std::endl;
+    return 0;
+} // Something about making it runnable through the terminal, not sure. 
