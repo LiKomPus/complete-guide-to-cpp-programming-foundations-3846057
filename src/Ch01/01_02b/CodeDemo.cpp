92a5804e8b3dab391b461e6be81abcb152bf8356
LiKomPus <56884876+LiKomPus@users.noreply.github.com>
2026-05-06 10:30:53 +0000

    Modifcations

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 01_02
+// Exercise 01_02 with modifications
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main() {
+    // cout, send text to terminal
+    // endl, end line
+    std::cout << "Hello world!" << std::endl;
+    std::cout << std::endl;
+    return 0;
+}
