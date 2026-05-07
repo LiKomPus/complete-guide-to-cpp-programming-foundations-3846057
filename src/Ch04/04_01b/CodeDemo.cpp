3bb7b8a008e9f809b6284747380a26c506d7cfcc
LiKomPus <56884876+LiKomPus@users.noreply.github.com>
2026-05-07 11:18:04 +0000

    Solved

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -1,10 +1,32 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 04_01
+// Exercise 04_01 with modifcations
 // Arrays, by Eduardo Corpeño 
 
 #include <iostream>
+// #define LEVEL_COUNT 4
 
 int main(){
+    const size_t LEVEL_COUNT = 4;
+    int enemies[LEVEL_COUNT];
+    float levelDifficulty[] = {1.0, 3.5, 6.0, 10.5};
+
+    enemies[0] = 10;
+    enemies[1] = 15;
+    enemies[2] = 20;
+    enemies[3] = 25;
+
+    std::cout << LEVEL_COUNT << std::endl;
+    std::cout << enemies[0] << std::endl;
+    std::cout << enemies[1] << std::endl;
+    std::cout << enemies[2] << std::endl;
+    std::cout << enemies[3] << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << levelDifficulty[0] << std::endl;
+    std::cout << levelDifficulty[1] << std::endl;
+    std::cout << levelDifficulty[2] << std::endl;
+    std::cout << levelDifficulty[3] << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
