10dd3831f54f6d804a469286dd20a524acba70f8
LiKomPus <56884876+LiKomPus@users.noreply.github.com>
2026-05-20 10:12:04 +0000

    with modifications

--- src/Ch05/05_04b/CodeDemo.cpp
+++ src/Ch05/05_04b/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 05_04
+// Exercise 05_04 with modifications
 // While Loops, by Eduardo Corpeño 
 
 #include <iostream>
@@ -8,7 +8,20 @@
 int main(){
     std::vector<int> playerScores = {12, 25, 31, 47, 58};
     
-    std::vector<int>::iterator scorePtr = playerScores.begin();
+    auto scorePtr = playerScores.begin();
+
+    while (scorePtr != playerScores.end()) {
+        std::cout << *scorePtr << " ";
+        scorePtr = next(scorePtr, 1);
+    }
+    std::cout << std::endl;
+
+    int i = 0;
+    do {
+        std::cout << playerScores[i] << " ";
+        i++;
+    } while(i < playerScores.size());
+    
     
     std::cout << std::endl << std::endl;
     return (0);
