2162fe8777bf627490a8d610acbb5df288aaf35e
LiKomPus <56884876+LiKomPus@users.noreply.github.com>
2026-05-20 11:13:22 +0000

    Mods

--- src/Ch06/06_02b/CodeDemo.cpp
+++ src/Ch06/06_02b/CodeDemo.cpp
@@ -1,16 +1,36 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 06_02
+// Exercise 06_02 with modifcations
 // Passing Values to a Function, by Eduardo Corpeño 
 
 #include <iostream>
 
+int square(int x) {
+    x *= x;
+    return x;
+}
+
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void swap(int& x, int& y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
 int main(){
     int a = 9, b;
-    // TODO: square
+
+    b = square(a);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
+
+    swap(&a, &b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
+
+    swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;
