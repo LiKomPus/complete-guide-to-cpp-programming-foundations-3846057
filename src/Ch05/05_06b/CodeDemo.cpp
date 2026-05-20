d7a0a4a5ea70b838804d68cff2d5404a26422d92
LiKomPus <56884876+LiKomPus@users.noreply.github.com>
2026-05-20 10:18:44 +0000

    with modifications

--- src/Ch05/05_06b/CodeDemo.cpp
+++ src/Ch05/05_06b/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 05_06
+// Exercise 05_06 with modifications
 // Traditional For Loops, by Eduardo Corpeño 
 
 #include <iostream>
@@ -8,6 +8,14 @@
 int main(){
     std::vector<int> lapTimes = {92, 85, 88, 90, 87};
     float average;
+
+    average = 0.0f;
+    for(int i = 0; i < lapTimes.size(); i++) {
+        average += lapTimes[i];
+    }
+    average /= lapTimes.size();
+
+    std::cout << "The average is " << average << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
